3dfd8c25b9e569c6c963e138e051fbcbf4e8657c
Sahar Yaccov <saharyaccov@gmail.com>
2026-03-12 01:19:05 +0200

    Delete Freedom-Force-The-Reckoning/Freedom-Force-The-Reckoning-game directory

--- Freedom-Force-The-Reckoning/Freedom-Force-The-Reckoning-game/src/screens/__init__.py
+++
@@ -1,5 +0,0 @@
-# screens package
-from .title_screen    import TitleScreen
-from .rules_screen    import RulesScreen
-from .mobile_controls import MobileControls
-from .about_screen    import AboutScreen
